2d7afc47d65569e19dc9cc50dddefeef0f817bae
qlows <79415956+qlows@users.noreply.github.com>
2022-11-27 15:05:38 -0800

    Added home and details screen

--- lab8/App.js
+++ lab8/App.js
@@ -9,6 +9,7 @@ import {
   TextInput,
   FlatList,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 import Constants from 'expo-constants';
 
@@ -17,27 +18,29 @@ import AssetExample from './components/AssetExample';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const DATA = [
   {
     id: '1',
-    title: 'First Item',
-    author: 'Jane Doe',
+    username: 'First Item',
+    name: 'Jane Doe',
   },
   {
     id: '2',
-    title: 'Second Item',
-    author: 'Mark Sloan',
+    username: 'Second Item',
+    name: 'Mark Sloan',
   },
   {
     id: '3',
-    title: 'Third Item',
-    author: 'Dan Brown',
+    username: 'Third Item',
+    name: 'Dan Brown',
   },
   {
     id: '4',
-    title: 'Fourth Item',
-    author: 'Nick Lee',
+    username: 'Fourth Item',
+    name: 'Nick Lee',
   },
 ];
 
@@ -50,48 +53,83 @@ function Item({ title, author, backgroundColor, textColor, onPress }) {
   );
 }
 
-// <Item title={DATA[0].title} author={DATA[0].author}/>
-// <Item title={DATA[1].title} author={DATA[1].author}/>
-export default function App() {
-  const [listData, setListData] = React.useState(DATA);
-  const [refreshCnt, setRefresh] = React.useState(0);
+function HomeScreen(navigation) {
+  const [refresh, setRefresh] = React.useState(0);
   const [selectedId, setSelectedId] = React.useState(null);
+  const [isLoading, setLoading] = React.useState(true);
+  const [list, setData] = React.useState(DATA);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    getUsers(), [];
+  });
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        title="Add"
-        onPress={() => {
-          DATA.push({
-            id: (Math.random() * 100).toString(),
-            author: 'Auth' + (Math.random() * 100).toString().substr(5),
-            title: 'Auto' + (Math.random() * 100).toString().substr(5),
-          });
-          setListData(DATA);
-          setRefresh(refreshCnt + 1);
-        }}
-      />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={list}
+          renderItem={({ item }) => {
+            const backgroundColor =
+              item.id === selectedId ? '#800000' : '#D2B48C';
+            const color = item.id === selectedId ? 'white' : 'black';
 
-      <FlatList
-        data={listData}
-        renderItem={({ item }) => {
-          const backgroundColor =
-            item.id === selectedId ? '#800000' : '#D2B48C';
-          const color = item.id === selectedId ? 'white' : 'black';
-          return (
-            <Item
-              title={item.title}
-              author={item.author}
-              onPress={() => setSelectedId(item.id)}
-              backgroundColor={{ backgroundColor }}
-              textColor={{ color }}
-            />
-          );
-        }}
-        keyExtractor={(item) => item.id}
-        extraData={listData}
-      />
+            return (
+              <Item
+                title={item.username}
+                author={item.name}
+                onPress={() => navigation.navigate('Details', { item })}
+                backgroundColor={{ backgroundColor }}
+                textColor={{ color }}
+              />
+            );
+          }}
+          keyExtractor={(item) => item.id}
+          extraData={refresh}
+        />
+      )}
     </SafeAreaView>
+  );
+}
+
+function DetailsScreen(navigation, route) {
+  return (
+    <>
+      <Text>{route,params.item.name}</Text>
+      <Text>{route,params.item.username}</Text>
+      <Text>{route,params.item.email}</Text>
+      <Text>{route,params.item.phone}</Text>
+    </>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
